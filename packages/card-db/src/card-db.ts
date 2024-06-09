fea65728052eeabbbb9918ee0220d9410d475609
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-06-09 18:13:35 +0330

    feat(card-db): node

--- packages/card-db/src/card-db.ts
+++ packages/card-db/src/card-db.ts
@@ -1,11 +1,63 @@
-import {serve} from '@hono/node-server';
+import {GecutLogger} from '@gecut/logger';
+import {KartbookDbConnector} from '@gecut/kartbook-db-connector';
+import http from 'node:http';
 
 import config from './config.js';
-import {db, logger, app} from './core.js';
-import './routes/cards.js';
 
-app.get('/', (c) => c.redirect('/server-info'));
-app.get('/server-info', (c) => c.text('Gecut Web Server', 200));
+const logger = new GecutLogger('card-db');
+const db = new KartbookDbConnector(config.DATABASE.URI, logger.sub('db'));
+
+const server = http.createServer(async (request, response) => {
+  if (!request.url) {
+    response.writeHead(400, 'BAD REQUEST');
+    return response.end('BAD REQUEST');
+  }
+
+  const slug = request.url.split('/').pop();
+
+  if (!slug) {
+    response.writeHead(400, 'BAD REQUEST');
+    return response.end('BAD REQUEST');
+  }
+
+  const card = await db.$Card
+    .findOne({
+      slug: slug,
+    })
+    .populate('owner');
+
+  if (!card) {
+    response.writeHead(404, {'Content-Type': 'application/json'});
+
+    return response.end(
+      JSON.stringify({
+        ok: false,
+        data: {
+          error: 'card-not-found',
+          message: 'Card Not Found',
+        },
+      }),
+    );
+  }
+
+  if (card.disabled) {
+    response.writeHead(403, {'Content-Type': 'application/json'});
+
+    return response.end(
+      JSON.stringify({
+        ok: false,
+        data: {
+          error: 'card-disabled',
+          message: 'Card Disabled',
+        },
+      }),
+    );
+  }
+
+  response.writeHead(200, {'Content-Type': 'application/json'});
+
+  return response.end(JSON.stringify(card));
+});
 
 db.connect().then(async () => {
   logger.other?.('DB Connected');
@@ -14,14 +66,9 @@ db.connect().then(async () => {
 
   logger.other?.('DB Initialized');
 
-  serve(
-    {
-      fetch: app.fetch,
-      hostname: config.SERVER.HOST,
-      port: config.SERVER.PORT,
-    },
-    console.log,
-  );
+  server.listen(config.SERVER.PORT, config.SERVER.HOST, () => {
+    console.log(`Server running on port ${config.SERVER.HOST}:${config.SERVER.PORT}`);
+  });
 
   logger.other?.('Server Launched', {devMode: logger.devMode});
 });
