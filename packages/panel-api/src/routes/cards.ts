a2ecd94d4e71a540604d3fa40d9779f6dc7a0b0b
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-06-27 18:17:34 +0330

    feat(panel-api): cards

--- packages/panel-api/src/routes/cards.ts
+++ packages/panel-api/src/routes/cards.ts
@@ -4,9 +4,11 @@ import type {CardData} from '@gecut/kartbook-types';
 
 const cards = router({
   all: $UserProcedure.query((opts) => {
-    const cards = db.$Card.find({
-      owner: opts.ctx.user,
-    });
+    const cards = db.$Card
+      .find({
+        owner: opts.ctx.user,
+      })
+      .populate('owner');
 
     return cards as unknown as CardData[];
   }),
