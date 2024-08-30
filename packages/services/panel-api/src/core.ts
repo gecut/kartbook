bc14608b0730b32455b497388dc748eaf5dcf8ff
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-08-30 18:54:27 +0330

    feat(services/panel-api): database options

--- packages/services/panel-api/src/core.ts
+++ packages/services/panel-api/src/core.ts
@@ -67,7 +67,10 @@ export const $UserProcedure = $PublicProcedure.use(authorizeMiddleware);
 export const $AdminProcedure = $UserProcedure.use(adminMiddleware);
 
 export const logger = new GecutLogger('panel-api');
-export const db = new KartbookDbConnector(config.DATABASE.URI, logger.sub('db-connector'));
+export const db = new KartbookDbConnector(config.DATABASE.URI, logger.sub('db-connector'), {
+  appName: 'panel-api',
+  loadBalanced: true,
+});
 export const microSMS = new KavenegarApi(config.KAVENEGAR_TOKEN, logger);
 export const zibalGateway = new ZibalGateWay(config.ZIBAL.PAYMENT, logger);
 
