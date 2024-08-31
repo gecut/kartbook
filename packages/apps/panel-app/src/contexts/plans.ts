f01fb5e18219d3134011a9adb94f960d309654a5
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-09-01 03:12:31 +0330

    fix(apps/panel-app): load plans

--- packages/apps/panel-app/src/contexts/plans.ts
+++ packages/apps/panel-app/src/contexts/plans.ts
@@ -1,6 +1,5 @@
 import {ContextSignal} from '@gecut/signal';
 
-import {userContext} from './user.js';
 import {client} from '../client/index.js';
 
 import type {PlanData} from '@gecut/kartbook-types';
@@ -8,9 +7,7 @@ import type {PlanData} from '@gecut/kartbook-types';
 export const plansContext = new ContextSignal<PlanData[]>('plans', 'IdleCallback');
 
 export async function loadPlans() {
-  userContext.requireValue().then(() => {
-    client.plan.all.query().then((plans) => {
-      plansContext.value = plans;
-    });
+  client.plan.all.query().then((plans) => {
+    plansContext.value = plans;
   });
 }
