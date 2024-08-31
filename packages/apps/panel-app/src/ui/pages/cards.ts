78186550c743a11745d189c64d1a92e8e737cef7
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-09-01 03:16:20 +0330

    refactor(apps/panel-app): cards ui

--- packages/apps/panel-app/src/ui/pages/cards.ts
+++ packages/apps/panel-app/src/ui/pages/cards.ts
@@ -1,6 +1,7 @@
-import {gecutButton} from '@gecut/components';
+import {gecutButton, icon} from '@gecut/components';
 import {gecutContext} from '@gecut/lit-helper/directives/context.js';
 import {repeat} from 'lit/directives/repeat.js';
+import {when} from 'lit/directives/when.js';
 import {html} from 'lit/html.js';
 
 import {cardsContext, selectedCardContext} from '../../contexts/cards.js';
@@ -8,34 +9,54 @@ import {$CardDetail} from '../components/card-detail.js';
 import {$CardListItem} from '../components/card-list-item.js';
 import {resolveRouterPath} from '../router/index.js';
 
-import SolarAddCircleBoldDuotone from '~icons/solar/add-circle-bold-duotone';
+import LineMdPlus from '~icons/line-md/plus';
+import SolarCardLineDuotone from '~icons/solar/card-line-duotone';
 
 export function $CardsPage() {
-  return gecutContext(
-    cardsContext,
-    (cards) => html`
-      <div class="flex flex-col py-4 gap-4">${gecutContext(selectedCardContext, $CardDetail)}</div>
+  const _$NewCardButton = gecutButton({
+    type: 'filled',
+    label: 'ایجاد کارت جدید',
+    href: resolveRouterPath('cards/create'),
+    icon: {
+      svg: LineMdPlus,
+    },
+  });
 
-      <div class="flex flex-col gap-2 py-4">
-        <h2 class="text-titleMedium text-onSurfaceVariant mb-2">کارت های شما</h2>
-        ${gecutContext(selectedCardContext, (selectedCard) =>
-          repeat(
-            cards,
-            (card) => card._id,
-            (card, index) => $CardListItem(card, index, selectedCard),
-          ),
-        )}
+  return gecutContext(cardsContext, (cards) =>
+    when(
+      cards.length > 0,
+      () => html`
+        <div class="flex flex-col py-4 gap-4">${gecutContext(selectedCardContext, $CardDetail)}</div>
 
-        <div class="mt-2"></div>
-        ${gecutButton({
-          type: 'filled',
-          label: 'ایجاد کارت جدید',
-          href: resolveRouterPath('cards/create'),
-          trailingIcon: {
-            svg: SolarAddCircleBoldDuotone,
-          },
-        })}
-      </div>
-    `,
+        <div class="flex flex-col gap-2 py-4">
+          <h2 class="text-titleMedium text-onSurfaceVariant mb-2">کارت های شما</h2>
+          ${gecutContext(selectedCardContext, (selectedCard) =>
+            repeat(
+              cards,
+              (card) => card._id,
+              (card, index) => $CardListItem(card, index, selectedCard),
+            ),
+          )}
+
+          <div class="mt-2"></div>
+          ${_$NewCardButton}
+        </div>
+      `,
+      () => html`
+        <main class="flex flex-1 flex-col max-w-md mx-auto page-modal has-top-bar pb-20 px-4 h-full w-full !z-sticky">
+          <div class="h-full w-full flex flex-col items-center justify-center [&>.gecut-button]:w-full
+                      *:animate-fadeInSlide">
+            <i class="[&>.gecut-icon]:text-[6rem] text-primary !animate-bounce">
+              ${icon({
+                svg: SolarCardLineDuotone,
+              })}
+            </i>
+            <h1 class="text-titleMedium mt-2">کارتی برای نمایش وجود ندارد</h1>
+            <p class="text-bodySmall text-onSurface mt-1 mb-6">همین حالا یکی از کارت های خود را آنلاین کنید</p>
+            ${_$NewCardButton}
+          </div>
+        </main>
+      `,
+    ),
   );
 }
