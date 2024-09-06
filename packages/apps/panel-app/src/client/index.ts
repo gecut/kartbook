7181ee17cecfb227683570edcac47135bd1e35d0
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-09-07 02:16:27 +0330

    refactor(apps/panel-app): client middleware

--- packages/apps/panel-app/src/client/index.ts
+++ packages/apps/panel-app/src/client/index.ts
@@ -1,13 +1,24 @@
-import {createTRPCProxyClient, httpBatchLink} from '@trpc/client';
+import {GecutLogger} from '@gecut/logger';
+import {createTRPCProxyClient, httpLink} from '@trpc/client';
 
 import {envvm} from '../utilities/envvm.js';
 import {sbm} from '../utilities/sbm.js';
 
 import type {AppRouter} from '@gecut/kartbook-panel-api/panel-api.js';
 
+const fetchLogger = new GecutLogger('createTRPCProxyClient.httpLink');
+
+const errors: Record<string, string> = {
+  'discount-not-found': 'تخفیف یافت نشد',
+  'discount-not-applicable-to-this-plan': 'این تخفیف برای این پلن قابل استفاده نیست',
+  'discount-usage-limit-exceeded': 'تعداد استفاده از این تخفیف به پایان رسیده است',
+  'discount-not-yet-started': 'این تخفیف هنوز فعال نشده است',
+  'discount-has-expired': 'این تخفیف منقضی شده است',
+};
+
 export const client = createTRPCProxyClient<AppRouter>({
   links: [
-    httpBatchLink({
+    httpLink({
       url: import.meta.env.API_URL ?? 'https://api.panel.kartbook.ir',
       headers: () => ({
         Authorization: `Bearer ${envvm.get('user-token')}`,
@@ -15,13 +26,19 @@ export const client = createTRPCProxyClient<AppRouter>({
       fetch: (...opts) => {
         return fetch(...opts)
           .then(async (response) => {
-            if (response.ok) return response;
+            if (response.ok) {
+              fetchLogger.methodArgs?.('fetch', {response});
+
+              return response;
+            }
 
             const result = await response.clone().json();
 
-            if (result.error.message) {
+            fetchLogger.methodArgs?.('fetch', {response, result});
+
+            if (typeof result.error.message === 'string') {
               sbm.notify({
-                message: result.error.message,
+                message: errors[String(result.error.message)] || result.error.message,
                 close: true,
               });
             }
@@ -29,7 +46,7 @@ export const client = createTRPCProxyClient<AppRouter>({
             return response;
           })
           .catch((error) => {
-            console.error('trpc_fetch_error', error);
+            fetchLogger.error('fetch', error);
 
             sbm.notify({
               message: 'اتصال به سرور با مشکل روبه‌رو شد. لطفا اتصال خود را بررسی کرده و دوباره تلاش کنید.',
