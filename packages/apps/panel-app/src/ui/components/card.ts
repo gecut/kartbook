79abfe84e2b96a1bea9ec613ddad50bc37cbbcd7
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-09-09 16:03:28 +0330

    fix(apps/panel-app): image clone

--- packages/apps/panel-app/src/ui/components/card.ts
+++ packages/apps/panel-app/src/ui/components/card.ts
@@ -59,7 +59,9 @@ export function $CardRenderer(
       <span class="absolute inset-0 opacity-20 bg-cover bg-[url('/card-bg.webp')] z-[3]"></span>
 
       <div class="absolute inset-0 z-[5] flex flex-col pt-3 pb-6 px-6">
-        <div class="size-20 flex items-center justify-center *:h-full *:w-full *:object-contain">${bank.image}</div>
+        <div class="size-20 flex items-center justify-center *:h-full *:w-full *:object-contain">
+          ${bank.image?.cloneNode(true)}
+        </div>
 
         <div class="grow"></div>
 
